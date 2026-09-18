039c6ac9650524cf8e5bb420acb1b29691add3aa
Lucia Fuertes <lucia.fuertes.06@gmail.com>
2026-09-18 10:47:51 +0200

    Primeros avances.

---
+++ DesayunoParalelo/DesayunoParalelo/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+
+Console.Title = "Agenda de contactos - DWES-01-Agenda";
+Console.OutputEncoding = Encoding.UTF8;
+Console.Clear();
+
+// Programa principal
+Main();
+
+// Limpieza de logs y salida
+Console.WriteLine("\n⌨️ Presiona una tecla para salir...");
+Console.ReadKey();
+
+// Programa principal
+void Main() {
+
+
+    
+    
+}
